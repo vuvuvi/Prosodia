8b27b06e93ebf92ccb1f375cdc7b70bee26b56c0
Thibaut <thibaut.baronheid@gmail.com>
2022-09-20 16:16:47 +0200

    MergingConflict

---
+++ Unity/ProsodiaProto/Assets/_/Scripts/LightUpAction.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightUpAction : ActionHolder
+{
+  private Material material;
+  private MeshRenderer meshRenderer;
+  private Color baseColor;
+  public float LitUpTime = 1;
+  protected void OnEnable()
+  {
+    Action = LightUp;
+    meshRenderer = gameObject.GetComponent<MeshRenderer>();
+    material = new Material(meshRenderer.material);
+    meshRenderer.material = material;
+    baseColor = material.color;
+  }
+  private void LightUp(int note)
+  {
+    var newColor = NoteInfoProvider.GetNoteColor(note);
+    if (material.color == newColor)
+    {
+      return;
+    }
+    material.color = newColor;
+    StartCoroutine(LightDownCoroutine());
+  }
+
+  private IEnumerator LightDownCoroutine()
+  {
+    yield return new WaitForSeconds(LitUpTime);
+    LightDown();
+    yield return null;
+  }
+
+  private void LightDown()
+  {
+    if (!IsValid)
+      material.color = baseColor;
+  }
+  protected override void Validate()
+  {
+    LightDown();
+  }
+}
