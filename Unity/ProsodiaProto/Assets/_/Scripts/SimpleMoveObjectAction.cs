6e1a920ba70d265b8ac48a60ff669174013abd68
Liador <jean.lousberg@yahoo.Fr>
2022-10-05 13:22:24 +0200

    Fixed the element movement on note played

--- Unity/ProsodiaProto/Assets/_/Scripts/SimpleMoveObjectAction.cs
+++ Unity/ProsodiaProto/Assets/_/Scripts/SimpleMoveObjectAction.cs
@@ -2,25 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimpleMoveObjectAction : ActionHolder
+public class SimpleMoveObjectAction : NoteActionHolder
 {
     public Transform ObjectToMove;
     public Transform Destination;
     public float Speed = 1;
     private bool isMoving;
+    protected void OnEnable()
+    {
+        Action = Move;
+    }
+    protected override void Validate()
+    {
 
-    private void Move()
+    }
+
+    private void Move(int i)
     {
         isMoving = true;
     }
 
-    protected override void CreateAction()
-    {
-        Action = () =>
-        {
-            Move();
-        };
-    }
     private void Update()
     {
         if (Vector3.Distance(ObjectToMove.position, Destination.position) < 0.2f)
