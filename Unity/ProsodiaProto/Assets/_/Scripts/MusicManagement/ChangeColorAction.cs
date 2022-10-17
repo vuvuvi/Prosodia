474e46778de51c6a8d890d80ae7d646b304d588e
Liador <jean.lousberg@yahoo.fr>
2022-10-17 21:18:37 +0200

    fixed rock puzzle not having proper feed back
    test adding color to sea organ

--- Unity/ProsodiaProto/Assets/_/Scripts/MusicManagement/ChangeColorAction.cs
+++ Unity/ProsodiaProto/Assets/_/Scripts/MusicManagement/ChangeColorAction.cs
@@ -1,46 +1,57 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ChangeColorAction : NoteActionHolder
 {
-  private Material material;
-  private MeshRenderer meshRenderer;
-  private Color baseColor;
-  public float LitUpTime = 1;
-  
-  protected void OnEnable()
-  {
-    Action = LightUp;
-    meshRenderer = gameObject.GetComponentInChildren<MeshRenderer>();
-    material = new Material(meshRenderer.material);
-    meshRenderer.material = material;
-    baseColor = material.color;
-  }
-  private void LightUp(int note)
-  {
-    var newColor = NoteInfoProvider.GetNoteColor(note);
-    if (material.color == newColor)
+    private List<Material> materials;
+    private MeshRenderer meshRenderer;
+    private List<Color> baseColors;
+    public float LitUpTime = 1;
+
+    protected void OnEnable()
     {
-      return;
+        Action = LightUp;
+        meshRenderer = gameObject.GetComponentInChildren<MeshRenderer>();
+        materials = new List<Material>();
+        baseColors = new List<Color>();
+        var mats = meshRenderer.materials.ToList();
+        foreach (var mat in mats)
+        {
+            materials.Add(new Material(mat));
+            baseColors.Add(mat.color);
+        }
+        meshRenderer.materials = materials.ToArray();
     }
-    material.color = newColor;
-    StartCoroutine(LightDownCoroutine());
-  }
+    private void LightUp(int note)
+    {
+        var newColor = NoteInfoProvider.GetNoteColor(note);
+        if (materials[0].color == newColor)
+        {
+            return;
+        }
+        materials.ForEach(m => m.color = newColor);
+        StartCoroutine(LightDownCoroutine());
+    }
 
-  private IEnumerator LightDownCoroutine()
-  {
-    yield return new WaitForSeconds(LitUpTime);
-    LightDown();
-    yield return null;
-  }
+    private IEnumerator LightDownCoroutine()
+    {
+        yield return new WaitForSeconds(LitUpTime);
+        LightDown();
+        yield return null;
+    }
 
-  private void LightDown()
-  {
-    if (!IsValid)
-      material.color = baseColor;
-  }
-  protected override void Validate()
-  {
-    LightDown();
-  }
+    private void LightDown()
+    {
+        if (!IsValid)
+            for (int i = 0; i < materials.Count; i++)
+            {
+                materials[i].color = baseColors[i];
+            }
+    }
+    protected override void Validate()
+    {
+        LightDown();
+    }
 }
